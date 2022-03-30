9f1ce3d55840f192f13ee7f9f760cdd4c8dd7a31
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-03-30 19:29:14 +0100

    Subset processing started

--- code/data_processing/get_subdatasets_topconcepts.py
+++ code/data_processing/get_subdatasets_topconcepts.py
@@ -1,6 +1,9 @@
 # System Imports
 import os
 import sys
+import tqdm
+import numpy as np
+import pandas as pd
 
 # Append current working directory to PATH to export stuff outside this folder
 if os.getcwd() not in sys.path:
@@ -25,6 +28,54 @@ TOP_K_CONCEPTS = 100
 concept_dict_name_to_idx, concept_dict_idx_to_name = get_concepts_dicts(data_dir=DATA_DIR, concepts_csv=CONCEPTS_CSV)
 
 
-# Get top-k most frequent concepts
+# Get top-k most frequent concepts (in training set)
 _, _, _, most_frequent_concepts, _, _ = get_statistics(filename=os.path.join(DATA_DIR, CONCEPTS_TRAIN), top_k_concepts=TOP_K_CONCEPTS)
-print(most_frequent_concepts)
+# print(most_frequent_concepts)
+
+
+# Open train subset
+concepts_train_df = pd.read_csv(os.path.join(DATA_DIR, CONCEPTS_TRAIN), sep="\t")
+
+# Iterate through train subset and generate a new subset with these concepts
+new_train_subset = list()
+
+for index, row in tqdm.tqdm(concepts_train_df.iterrows()):
+    
+    # Parse image and concepts
+    image = row["ID"]
+    concepts = row["cuis"].split(';')
+    # print(concepts)
+
+    # Populate data array with ones where we have concepts
+    for c in concepts:
+        if c in most_frequent_concepts:
+            new_train_subset.append([image, concepts])
+            break
+    
+print(len(new_train_subset))
+
+
+
+# Open validation subset
+concepts_val_df = pd.read_csv(os.path.join(DATA_DIR, CONCEPTS_VAL), sep="\t")
+
+# Iterate through train subset and generate a new subset with these concepts
+new_val_subset = list()
+
+for index, row in tqdm.tqdm(concepts_val_df.iterrows()):
+    
+    # Parse image and concepts
+    image = row["ID"]
+    concepts = row["cuis"].split(';')
+    # print(concepts)
+
+    # Populate data array with ones where we have concepts
+    for c in concepts:
+        if c in most_frequent_concepts:
+            new_val_subset.append([image, concepts])
+            break
+    
+print(len(new_val_subset))
+
+
+print("Finished.")
