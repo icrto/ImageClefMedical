19b30a0505259a89750cd4263febb3325adc1d97
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-05-03 15:53:35 +0100

    Find the bug on model agg.

--- concept_detection/semantic_baseline/model_prediction_aggregation.py
+++ concept_detection/semantic_baseline/model_prediction_aggregation.py
@@ -25,18 +25,20 @@ test_path = "test"
 
 
 # Iterate through subsets
-for subset in [validation_path, test_path]:
+for subset in [test_path, validation_path]:
 
     # Get directory
     eval_dir = os.path.join(results_path, semantic_baseline_path, subset)
 
     # Read directory
     csv_files = [c for c in os.listdir(eval_dir) if not c.startswith('.')]
+    csv_files = [c for c in csv_files if c not in ("test_agg.csv", "val_agg.csv")]
+
+    assert len(csv_files) == 9, "Check you directory."
 
     # Pre-create image lists
     eval_data = dict()
-    eval_images = list()
-    eval_concepts = list()
+
 
     # Go through all the .CSV files
     for csv in csv_files:
@@ -46,63 +48,68 @@ for subset in [validation_path, test_path]:
             df = pd.read_csv(os.path.join(eval_dir, csv), sep="\t")
         
         else:
-            df = pd.read_csv(os.path.join(eval_dir, csv), sep="|", header=None, names=["ID", "cuis"])
-        
-        # Convert .CSV into array
-        df_values = df.values
-        # print(df_values)
+            df = pd.read_csv(os.path.join(eval_dir, csv), sep="|", header=None)
+
+
 
         # Create image list
         for _, row in df.iterrows():
             if subset == "validation":
-                if row["ID"] not in eval_images:
-                    eval_images.append(row["ID"])
-                    eval_concepts.append(list())
+                if row["ID"] not in eval_data.keys():
+                    eval_data[row["ID"]] = list()
             
             else:
-                if row["ID"] not in eval_images:
-                    eval_images.append(row["ID"])
-                    eval_concepts.append(list())
+                if row[0] not in eval_data.keys():
+                    eval_data[row[0]] = list()
 
-        
-        # print(eval_images)
 
         # Append concepts (if different from 'None')
         for index, row in df.iterrows():
-            # print(value[1])
+
             if subset == "validation":
-                pred_concepts = str(row["cuis"]).split(';')
+                eval_data[row["ID"]] += str(row["cuis"]).split(';')
+            
+                for i, c in enumerate(eval_data[row["ID"]]):
+                    if c in ("None", "nan"):
+                        eval_data[row["ID"]].pop(i)
+                
+                # Remove duplicates if needed (we don't know why this happens)
+                eval_data[row["ID"]] = list(dict.fromkeys(eval_data[row["ID"]]))
             
             else:
-                pred_concepts = str(row["cuis"]).split(';')
+                eval_data[row[0]] += str(row[1]).split(';')
             
-            for c in pred_concepts:
-                if c not in ("None", "nan"):
-                    eval_concepts[index].append(c)
-            
-            # FIXME: Remove duplicates if needed (we don't know why this happens)
-            eval_concepts[index] = list(dict.fromkeys(eval_concepts[index]))
-        
-        # print(eval_concepts)
-    
-    # print(eval_concepts)
+                for i, c in enumerate(eval_data[row[0]]):
+                    if c in ("None", "nan"):
+                        eval_data[row[0]].pop(i)
+                
+                # Remove duplicates if needed (we don't know why this happens)
+                eval_data[row[0]] = list(dict.fromkeys(eval_data[row[0]]))
+
 
     # Process concept lists
-    for index, value in enumerate(eval_concepts):
+    for key, value in eval_data.items():
         # Add the valid concepts
         predicted_concepts = ""
         for c in value:
             predicted_concepts += f"{c};"
         
-        eval_concepts[index] = predicted_concepts[:-1]
-    
-    # print(eval_concepts)
+        eval_data[key] = predicted_concepts[:-1]
+
 
     # Convert this data into a DataFrame
-    eval_data["ID"] = eval_images
-    eval_data["cuis"] = eval_concepts
-    evaluation_df = pd.DataFrame(data=eval_data)
+    df_dict = dict()
+    df_dict["ID"] = list()
+    df_dict["cuis"] = list()
+    for key, value in eval_data.items():
+        df_dict["ID"].append(key)
+        df_dict["cuis"].append(value)
+    
+    # print(df_dict)
+    evaluation_df = pd.DataFrame(data=df_dict)
 
+
+    # Select correct subsets
     if subset == "validation":
         
         # Open validation ground-truth
@@ -113,28 +120,70 @@ for subset in [validation_path, test_path]:
         # Open ground-truth data
         valid_df = pd.read_csv(os.path.join(data_dir, "concept_detection_valid.csv"), sep="\t")
 
+
+
+        # Join DataFrames to obtain a DataFrame with Ground-Truth and Predicted values as columns
+        predictions_df = evaluation_df.copy().merge(valid_df.copy(), on="ID", suffixes=('_pred', '_gt'))
+        predictions_df.dropna()
+        # print(predictions_df)
+
+
         # Generate label matrices
         # y_true
-        y_true = np.zeros((len(eval_images), len(concept_dict_name_to_idx)))
+        y_true = np.zeros((len(predictions_df), len(concept_dict_name_to_idx)))
         # print(valid_df['cuis'].values)
-        index = 0
-        for _, row in valid_df.iterrows():
-            if row['ID'] in eval_images:
-                    concepts = row['cuis'].split(';')
-                    for c in concepts:
-                        y_true[index, concept_dict_name_to_idx.get(c)] = 1
-                    
-                    index += 1
+        for index, row in predictions_df.iterrows():
+            concepts = row['cuis_gt'].split(';')
+            for c in concepts:
+                y_true[index, concept_dict_name_to_idx.get(c)] = 1
         
 
         y_pred = np.zeros_like(y_true)
-        for index, set_concepts in enumerate(eval_concepts):
-            concepts = set_concepts.split(';')
+        for index, row in predictions_df.iterrows():
+            concepts = row['cuis_pred'].split(';')
             for c in concepts:
                 y_pred[index, concept_dict_name_to_idx.get(c)] = 1
 
 
         print(f"/////////// Final Evaluation Report ////////////")
+        print(f"Exact Match Ratio: {metrics.accuracy_score(y_true, y_pred, normalize=True, sample_weight=None):.4f}")
+        print(f"Hamming loss: {metrics.hamming_loss(y_true, y_pred):.4f}")
+        print(f"Recall: {metrics.precision_score(y_true=y_true, y_pred=y_pred, average='samples'):.4f}")
+        print(f"Precision: {metrics.recall_score(y_true=y_true, y_pred=y_pred, average='samples'):.4f}")
+        print(f"F1 Measure: {metrics.f1_score(y_true=y_true, y_pred=y_pred, average='samples'):.4f}")
+
+
+
+        # Top-100 Report
+        sem_concepts_path = os.path.join(data_dir, "top100")
+        concept_dict_name_to_idx, concept_dict_idx_to_name, _ = get_concepts_dicts(data_dir=sem_concepts_path, concepts_csv="new_top100_concepts_sem.csv")
+        valid_csvpath = os.path.join(data_dir, "top100", "new_val_subset_top100_sem.csv")
+        valid_df = pd.read_csv(valid_csvpath, sep="\t")
+
+        # Join DataFrames to obtain a DataFrame with Ground-Truth and Predicted values as columns
+        predictions_df = evaluation_df.copy().merge(valid_df.copy(), on="ID", suffixes=('_pred', '_gt'))
+        predictions_df.dropna()
+        # print(predictions_df)
+
+
+        # Generate label matrices
+        # y_true
+        y_true = np.zeros((len(predictions_df), len(concept_dict_name_to_idx)))
+        # print(valid_df['cuis'].values)
+        for index, row in predictions_df.iterrows():
+            concepts = row['cuis_gt'].split(';')
+            for c in concepts:
+                y_true[index, concept_dict_name_to_idx.get(c)] = 1
+        
+
+        y_pred = np.zeros_like(y_true)
+        for index, row in predictions_df.iterrows():
+            concepts = row['cuis_pred'].split(';')
+            for c in concepts:
+                y_pred[index, concept_dict_name_to_idx.get(c)] = 1
+
+
+        print(f"/////////// Final Evaluation Report Top-100 ////////////")
         print(f"Exact Match Ratio: {metrics.accuracy_score(y_true, y_pred, normalize=True, sample_weight=None):.4f}")
         print(f"Hamming loss: {metrics.hamming_loss(y_true, y_pred):.4f}")
         print(f"Recall: {metrics.precision_score(y_true=y_true, y_pred=y_pred, average='samples'):.4f}")
