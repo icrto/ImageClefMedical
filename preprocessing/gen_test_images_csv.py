6950e9c622046dd0209e19ba3fae4e59d922c81f
icrto <icrtto@gmail.com>
2022-11-21 11:48:21 +0000

    added argparse

--- preprocessing/gen_test_images_csv.py
+++ preprocessing/gen_test_images_csv.py
@@ -1,11 +1,21 @@
 import os
 import pandas as pd
+import argparse
+parser = argparse.ArgumentParser(
+    description="Arguments to run the script.")
 
-data_dir = "/BARRACUDA8T/ImageCLEF2022/dataset/test"
+parser.add_argument("--datadir",
+                    type=str, required=True)
+args = parser.parse_args()
+
+if args.datadir.endswith('/'):
+    args.datadir = args.datadir[:-1]
+
 df = pd.DataFrame(columns=["ID"])
 
-df["ID"] = os.listdir(data_dir)
+df["ID"] = os.listdir(args.datadir)
 df["ID"] = df["ID"].apply(lambda x: x[:-4])
 
-df.to_csv(os.path.join(os.path.dirname(data_dir), "test_images.csv"), index=False)
-print(df)
+savepath = os.path.join(os.path.dirname(args.datadir), "test_images.csv")
+df.to_csv(savepath, index=False)
+print("Saved dataframe to: ", savepath)
